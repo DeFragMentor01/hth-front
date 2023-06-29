d263f0e20498b158d8050061f4ecc790054f7a2e
yerblich <yerblich@gmail.com>
2023-06-30 00:42:19 +0300

    initial

--- src/apis/registerDataApi.js
+++ src/apis/registerDataApi.js
@@ -1,9 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.API_BASE_URL}/api`,
-  });
-
-
-  
-  
+  baseURL: `${import.meta.env.API_BASE_URL}/api`,
+});
