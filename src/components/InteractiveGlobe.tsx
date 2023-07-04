2adc33c7808a392009eee53880b2560392700be5
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2023-07-04 23:36:06 +0300

    Update InteractiveGlobe.tsx

--- src/components/InteractiveGlobe.tsx
+++ src/components/InteractiveGlobe.tsx
@@ -41,133 +41,146 @@ const InteractiveGlobe: React.FC<InteractiveGlobeProps> = ({
   const [searchInput, setSearchInput] = useState("");
 
  useEffect(() => {
-    if (!mapContainerRef.current || !communitiesData.length) return;
+  if (!mapContainerRef.current || !communitiesData.length) return;
 
-    mapboxgl.accessToken = "pk.eyJ1IjoiYmFydWNoLWsiLCJhIjoiY2xpdDM3dnJqMGwxMDNobzc3emJtYndlaiJ9.mLMAW4ATqzmqjYW49Quo9Q";
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiYmFydWNoLWsiLCJhIjoiY2xpdDM3dnJqMGwxMDNobzc3emJtYndlaiJ9.mLMAW4ATqzmqjYW49Quo9Q";
 
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [0, 0] as LngLatLike,
-      zoom: 1,
-      maxBounds: [-180, -90, 180, 90] as LngLatBoundsLike,
-    });
+  const map = new mapboxgl.Map({
+    container: mapContainerRef.current,
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: [0, 0] as LngLatLike,
+    zoom: 1,
+    maxBounds: [-180, -90, 180, 90] as LngLatBoundsLike,
+  });
 
-    map.on("load", function () {
-      map.addSource("villages", {
-        type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: communitiesData
-            .filter(
-              (community) =>
-                typeof community.longitude === "number" &&
-                typeof community.latitude === "number"
-            )
-            .map((community) => ({
+  map.on("load", function () {
+    map.addSource("villages", {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: communitiesData
+          .filter(
+            (community) =>
+              typeof community.longitude === "number" &&
+              typeof community.latitude === "number"
+          )
+          .map((community) => {
+            const coordinates: [number, number] = [
+              community.longitude || 0,
+              community.latitude || 0,
+            ];
+            return {
               type: "Feature",
-              properties: { village_name: community.village_name, population: community.population },
-              geometry: { type: "Point", coordinates: [community.longitude, community.latitude] },
-            })),
-        },
-        cluster: true,
-        clusterMaxZoom: 14, 
-        clusterRadius: 50, 
-      });
-
-      map.addLayer({
-        id: "clusters",
-        type: "circle",
-        source: "villages",
-        filter: ["has", "point_count"],
-        paint: {
-          "circle-color": [
-            "step",
-            ["get", "point_count"],
-            "#51bbd6",
-            100,
-            "#f1f075",
-            750,
-            "#f28cb1",
-          ],
-          "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
-        },
-      });
-
-      map.addLayer({
-        id: "cluster-count",
-        type: "symbol",
-        source: "villages",
-        filter: ["has", "point_count"],
-        layout: {
-          "text-field": "{point_count_abbreviated}",
-          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-          "text-size": 12,
-        },
-      });
-
-      map.addLayer({
-        id: "unclustered-point",
-        type: "circle",
-        source: "villages",
-        filter: ["!", ["has", "point_count"]],
-        paint: {
-          "circle-color": "#11b4da",
-          "circle-radius": 4,
-          "circle-stroke-width": 1,
-          "circle-stroke-color": "#fff",
-        },
-      });
-
-      map.on("click", "clusters", function (e) {
-        var features = map.queryRenderedFeatures(e.point, {
-          layers: ["clusters"],
-        });
-        var clusterId = features[0].properties.cluster_id;
-        map.getSource("villages").getClusterExpansionZoom(
-          clusterId,
-          function (err, zoom) {
-            if (err) return;
-
-            map.easeTo({
-              center: features[0].geometry.coordinates,
-              zoom: zoom,
-            });
-          }
-        );
-      });
-
-      map.on("click", "unclustered-point", function (e) {
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var village_name = e.features[0].properties.village_name;
-
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        new mapboxgl.Popup()
-          .setLngLat(coordinates)
-          .setHTML(`Village: ${village_name}`)
-          .addTo(map);
-      });
-
-      map.on("mouseenter", "clusters", function () {
-        map.getCanvas().style.cursor = "pointer";
-      });
-
-      map.on("mouseleave", "clusters", function () {
-        map.getCanvas().style.cursor = "";
-      });
-
-      if (currentPopup) {
-        currentPopup.remove();
-      }
+              properties: {
+                village_name: community.village_name,
+                population: community.population,
+              },
+              geometry: {
+                type: "Point",
+                coordinates: coordinates,
+              },
+            };
+          }),
+      },
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50,
     });
 
-    return () => {
-      map.remove();
-    };
-  }, [communitiesData, searchTerm, darkMode]);
+    map.addLayer({
+      id: "clusters",
+      type: "circle",
+      source: "villages",
+      filter: ["has", "point_count"],
+      paint: {
+        "circle-color": [
+          "step",
+          ["get", "point_count"],
+          "#51bbd6",
+          100,
+          "#f1f075",
+          750,
+          "#f28cb1",
+        ],
+        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+      },
+    });
+
+    map.addLayer({
+      id: "cluster-count",
+      type: "symbol",
+      source: "villages",
+      filter: ["has", "point_count"],
+      layout: {
+        "text-field": "{point_count_abbreviated}",
+        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+        "text-size": 12,
+      },
+    });
+
+    map.addLayer({
+      id: "unclustered-point",
+      type: "circle",
+      source: "villages",
+      filter: ["!", ["has", "point_count"]],
+      paint: {
+        "circle-color": "#11b4da",
+        "circle-radius": 4,
+        "circle-stroke-width": 1,
+        "circle-stroke-color": "#fff",
+      },
+    });
+
+    map.on("click", "clusters", function (e) {
+      var features = map.queryRenderedFeatures(e.point, {
+        layers: ["clusters"],
+      });
+      var clusterId = features[0].properties.cluster_id;
+      map.getSource("villages").getClusterExpansionZoom(
+        clusterId,
+        function (err, zoom) {
+          if (err) return;
+
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom,
+          });
+        }
+      );
+    });
+
+    map.on("click", "unclustered-point", function (e) {
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var village_name = e.features[0].properties.village_name;
+
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(`Village: ${village_name}`)
+        .addTo(map);
+    });
+
+    map.on("mouseenter", "clusters", function () {
+      map.getCanvas().style.cursor = "pointer";
+    });
+
+    map.on("mouseleave", "clusters", function () {
+      map.getCanvas().style.cursor = "";
+    });
+
+    if (currentPopup) {
+      currentPopup.remove();
+    }
+  });
+
+  return () => {
+    map.remove();
+  };
+}, [communitiesData, searchTerm, darkMode]);
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
