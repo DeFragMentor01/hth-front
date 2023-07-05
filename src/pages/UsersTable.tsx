1ac75e1949f5dc1496c7d7c394a37dcf3e3b43e1
DeFragMentor01 <93282561+DeFragMentor01@users.noreply.github.com>
2023-07-05 16:06:14 +0300

    Update UsersTable.tsx

--- src/pages/UsersTable.tsx
+++ src/pages/UsersTable.tsx
@@ -42,6 +42,19 @@ const UsersTable: React.FC = () => {
 
   const [end, setEnd] = useState(pageSize);
   const tableContainerRef = useRef<HTMLDivElement>(null);
+  
+  function calculateAge(dateOfBirth: Date): number {
+  const today = new Date();
+  const birthDate = new Date(dateOfBirth);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age;
+}
 
   useEffect(() => {
     const fetchUrl = new URL(`${process.env.REACT_APP_BASE_URL}/users`);
